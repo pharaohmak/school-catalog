888eaf632a5ec6f01603dd2c52f25739d816b25a
Makram M. Ibrahim <61184060+pharaohmak@users.noreply.github.com>
2024-07-23 21:29:29 -0700

    refactored

--- main.js
+++ main.js
@@ -26,14 +26,13 @@ class School {
     }
 
     quickFacts() {
-        console.log(`${this.name} educates ${this.numberOfStudents} students, typically between the ages of ${this.level}.`);
+        return `${this.name} educates ${this.numberOfStudents} students, typically between the ages of ${this.level}.`;
     }
 
     static pickSubstituteTeacher(substituteTeachers) {
-        let rand = Math.floor(Math.random() * (substituteTeachers.length - 1));
+        let rand = Math.floor(Math.random() * substituteTeachers.length);
         return substituteTeachers[rand];
     }
-
 }
 
 class Primary extends School {
@@ -60,17 +59,25 @@ class High extends School {
     }
 
     get sportsTeams() {
-        console.log(this._sportsTeams);
+        return this._sportsTeams.join(', ');
     }
 }
 
-// testing
-
+// Create instances
 let lorraineHansbury = new Primary('Lorraine Hansbury', 514, 'Students must be picked up by a parent, guardian, or a family member over the age of 13.');
-
-lorraineHansbury.quickFacts();
-
-School.pickSubstituteTeacher(['Jamal Crawford', 'Lou Williams', 'J. R. Smith', 'James Harden', 'Jason Terry', 'Manu Ginobli']);
-
 let alSmith = new High('Al E. Smith', 415, ['Baseball', 'Basketball', 'Volleyball', 'Track and Field']);
-alSmith.sportsTeams;
+
+// Display school details
+const schoolDetails = document.getElementById('schoolDetails');
+schoolDetails.innerHTML = `
+    <p>${lorraineHansbury.quickFacts()}</p>
+    <p>Pickup Policy: ${lorraineHansbury.pickupPolicy}</p>
+    <p>${alSmith.quickFacts()}</p>
+    <p>Sports Teams: ${alSmith.sportsTeams}</p>
+`;
+
+// Pick substitute teacher
+document.getElementById('pickSubstitute').addEventListener('click', () => {
+    const substitute = School.pickSubstituteTeacher(['Jamal Crawford', 'Lou Williams', 'J. R. Smith', 'James Harden', 'Jason Terry', 'Manu Ginobli']);
+    document.getElementById('substituteTeacher').innerText = `Substitute Teacher: ${substitute}`;
+});
